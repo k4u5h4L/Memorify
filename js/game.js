34e6270ffb4c8379a5b5e93eb38d716a6840a751
Kaushal <kaushal.v.bhat@gmail.com>
2020-04-16 18:49:38 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,60 +4,71 @@ let cardArray = [
     {
         name: "cpp",
         img: "images/c++.png",
+        open: false,
     },
     {
         name: "cpp",
         img: "images/c++.png",
+        open: false,
     },
     {
         name: "python",
         img: "images/python.png",
+        open: false,
     },
     {
         name: "python",
         img: "images/python.png",
+        open: false,
     },
     {
         name: "c",
         img: "images/c.png",
+        open: false,
     },
     {
         name: "c",
         img: "images/c.png",
+        open: false,
     },
     {
         name: "js",
         img: "images/js.png",
+        open: false,
     },
     {
         name: "js",
         img: "images/js.png",
+        open: false,
     },
     {
         name: "flutter",
         img: "images/flutter.png",
+        open: false,
     },
     {
         name: "flutter",
         img: "images/flutter.png",
+        open: false,
     },
     {
         name: "tux",
         img: "images/tux.png",
+        open: false,
     },
     {
         name: "tux",
         img: "images/tux.png",
+        open: false,
     },
 ];
 
+// this function jumbles the tiles
 cardArray.sort(() => 0.5 - Math.random());
 
-for (let i = 0; i < document.querySelectorAll("img").length; i++) {
-    document.querySelectorAll("img")[i].addEventListener("click", function () {
-        //console.log(event);
-        loadImage(i);
-        //alert("Hello there");
+for (let i1 = 0; i1 < document.querySelectorAll("img").length; i1++) {
+    document.querySelectorAll("img")[i1].addEventListener("click", function () {
+        loadImage(i1);
     });
 }
 
@@ -65,4 +76,19 @@ function loadImage(index) {
     console.log(index);
     let card1 = document.querySelectorAll("img")[index];
     card1.setAttribute("src", cardArray[index].img);
+    cardArray[index].open = true;
+
+    checkImage(index);
+}
+
+function checkImage(index) {
+    //console.log("Picked " + index);
+    let temp = 0;
+    let tempArray = [];
+    for (let i = 0; i < cardArray.length; i++) {
+        if (cardArray[i].open == true) {
+            tempArray[temp++] = i;
+            console.log(tempArray.length);
+        }
+    }
 }
